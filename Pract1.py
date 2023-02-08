b8bf7d387ac778780ecb7ebe28a340daa46711e4
ILhaan <ilhaanrahim1@gmail.com>
2023-02-09 00:04:56 +0530

    prac1.txt

--- Pract1.py
+++ Pract1.py
@@ -1,3 +1,9 @@
+#1] To demonstrate classical Cycle.
+# Encrypt the given text Message with the help of following algorithm 
+ #   a. Replace each alphabet with its equivalent 7 bit ASCII Code
+ #   b. Swap the first 4 bits with last 4 bits for each alphabets
+ #   c. Write the hexadecimal equivalent of every 4 bits
+  
 def encrypt_message(message):
   ascii_codes = [bin(ord(char))[2:].zfill(8) for char in message]
   print("8-bit ASCII codes:", ascii_codes)
