17fe0ff4c4ea33113755e3301990b9af75b908c0
ILhaan <ilhaanrahim1@gmail.com>
2023-03-04 13:44:28 +0530

    Update Pract4.py

--- Pract4.py
+++ Pract4.py
@@ -1,33 +1,46 @@
-#To demonstrate transposition cypher 
-#Write a program to implement columnar transposition cypher
 import math
 
-def encrypt(plaintext, key):
-  # Check if key is valid
-  if not set(key) == set(str(i) for i in range(1, len(key) + 1)):
-    print("Error: Invalid key.")
-    return ""
-  ciphertext = ""
-  # Remove spaces from plaintext
-  plaintext = plaintext.replace(" ", "")
-  # Calculate number of rows needed for the grid
-  rows = math.ceil(len(plaintext) / len(key))
-  # Fill grid with plaintext
-  grid = [[""] * len(key) for _ in range(rows)]
-  i = 0
-  for row in range(rows):
-    for col in range(len(key)):
-      if i < len(plaintext):
-        grid[row][col] = plaintext[i]
-        i += 1
-  # Create ciphertext by reading columns in order of the key
-  for col in range(len(key)):
-    index = key.index(str(col + 1))
-    for row in range(rows):
-      ciphertext += grid[row][index]
+
+def encrypt_transposition_columnar(plaintext, key):
+  # Remove any spaces in the plaintext and convert to uppercase
+  plaintext = plaintext.replace(' ', '').upper()
+
+  # Calculate the number of columns in the table
+  num_cols = len(key)
+
+  # Calculate the number of rows in the table
+  num_rows = math.ceil(len(plaintext) / num_cols)
+
+  # Add padding to the plaintext if necessary
+  num_padding = num_rows * num_cols - len(plaintext)
+  plaintext += 'X' * num_padding
+
+  # Create a table with the plaintext and the key
+  table = [
+    list(plaintext[i:i + num_cols]) for i in range(0, len(plaintext), num_cols)
+  ]
+  table[0] = [key[i] for i in range(num_cols)]
+
+  # Sort the key and rearrange the columns accordingly
+  sorted_key = sorted(key)
+  sorted_table = [[0] * num_cols for _ in range(num_rows)]
+  for col in range(num_cols):
+    index = key.index(sorted_key[col])
+    for row in range(num_rows):
+      sorted_table[row][col] = table[row][index]
+
+  # Flatten the sorted table to produce the ciphertext
+  ciphertext = ''
+  for row in sorted_table:
+    ciphertext += ''.join(row)
+
   return ciphertext
 
-plaintext = input("Enter plaintext: ")
-key = input("Enter key: ")
-ciphertext = encrypt(plaintext, key)
-print("Ciphertext: " + ciphertext)
+
+# Get the plaintext and key from the user
+plaintext = input("Enter the plaintext: ")
+key = input("Enter the key: ")
+
+# Encrypt the plaintext
+ciphertext = encrypt_transposition_columnar(plaintext, key)
+print("Ciphertext:", ciphertext)
